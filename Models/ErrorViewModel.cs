759456407a1eb950cbb8c191263e514b9027abd0
harpermoore <chiayen@iu.edu>
2025-06-12 02:35:58 +0000

    Initial commit - ShelterAdminPortal ASP.NET Core MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShelterAdminPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
